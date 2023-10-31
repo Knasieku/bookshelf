543d97f59b045ecb07dfa8632a881f9b5c74baa0
Knasieku <knasieku@gmail.com>
2023-10-31 17:39:42 +0300

    created class borrower

---
+++ class.py
@@ -0,0 +1,9 @@
+class Borrower:
+    def __init__(self,username,password,confirm_password,email,location):
+        self.username=username
+        self.password=password
+        self.confirm_password=confirm_password
+        self.email=email
+        self.location=location
+    
+    
