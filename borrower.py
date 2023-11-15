3344bb5c8e057f21c90560b0eb85bf0306460001
Knasieku <knasieku@gmail.com>
2023-11-15 17:37:57 +0300

    added attendant class as a child class to borrower

--- borrower.py
+++ borrower.py
@@ -13,4 +13,14 @@ class Borrower:
     
     def delete_details(self):
         Borrower.borrower_details.remove(self)
-        
+
+class Attendant (Borrower):
+    def __init__(self, username, password, confirm_password, email, location,store_location):
+        super().__init__(username, password, confirm_password, email, location)
+        self.store_location=store_location
+
+    attendant_details=[]
+
+    def save_details(self):
+        return super().save_details()
+           
