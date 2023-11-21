bf814206bc667864714afc286dde9942aea6da09
Knasieku <knasieku@gmail.com>
2023-11-21 17:38:23 +0300

    added run.py and main function

---
+++ run.py
@@ -0,0 +1,14 @@
+# def main():
+ 
+#   prompt=input()
+#   if prompt=="Yes":
+#     print("Hey yes")
+
+#   if __name__== '__main__':
+#     main()
+
+def trial ():
+    name=input ()
+
+    print (name +  "\nhello")
+trial()
